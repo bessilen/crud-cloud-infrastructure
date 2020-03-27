b669ea1ecaa9c6a5bf3297432f38737fa77f7892
root <root@DESKTOP-6VHSN4Q.localdomain>
2020-03-27 11:42:18 +0300

    Initial commit

---
+++ src/CrudCloudInfrastructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrudCloudInfrastructure
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new CrudCloudInfrastructureStack(app, "CrudCloudInfrastructureStack");
+            app.Synth();
+        }
+    }
+}
